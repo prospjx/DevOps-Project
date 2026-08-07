86ad8c0e64d2142ea7a576f3b8f36765c46c6b8d
Prosper Olusegun-Joseph <prospergodsson@gmail.com>
2026-08-06 23:33:37 -0400

    CI-CD Yaml (Github Actions)

--- app/config.py
+++ app/config.py
@@ -1,15 +1,13 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     APP_NAME: str = "Task API"
     APP_VERSION: str = "1.0.0"
     ENVIRONMENT: str = "development"
     DEBUG: bool = True
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/postgres"
-
+    
     class Config:
-        env_file = ("app/.env", ".env")
-
+        env_file = ".env"
 
 settings = Settings()
